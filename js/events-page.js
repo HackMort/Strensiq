5e2a48a5df92f644f27ae1a4f863180f5a6df1e9
Anderson Castellon <anderson.castellon@jdoutstanding.com>
2023-06-14 00:29:09 -0600

    ✨ Add event filters

--- js/events-page.js
+++ js/events-page.js
@@ -1,3 +1,90 @@
+/**
+
+Represents a list of events with filtering functionality.
+/
+class EventList {
+/*
+Creates an instance of the EventList class.
+@param {NodeList} eventList - The list of events to filter.
+*/
+class EventList {
+  constructor (eventList) {
+    this.eventList = eventList
+  }
+
+  hideBorderBottom (event) {
+    event.setAttribute(
+      'style',
+      'border-bottom: none; margin-bottom: 0; padding-bottom: 0'
+    )
+  }
+
+  resetBorderBottom (event) {
+    event.removeAttribute('style')
+  }
+
+  /**
+   * It loops through all the events and if the event type is not the same as the
+   * type passed in, it adds the class event--hidden and removes the class
+   * event--showed. If the event type is the same as the type passed in, it removes
+   * the class event--hidden and adds the class event--showed
+   * @param type - the type of event you want to filter
+   */
+  filter (type) {
+    this.eventList.forEach(event => {
+      const { eventType } = event.dataset
+      if (eventType !== type) {
+        event.classList.add('event-card--hidden')
+        event.classList.remove('event-card--showed')
+      } else {
+        event.classList.remove('event-card--hidden')
+        event.classList.add('event-card--showed')
+      }
+    })
+
+    const showedList = Array.from(this.eventList).filter(event => {
+      return event.classList.contains('event-card--showed')
+    })
+
+    if (showedList && showedList.length > 0) {
+      const lastShowedEvent = showedList[showedList.length - 1]
+
+      showedList.forEach(event => {
+        this.resetBorderBottom(event)
+      })
+
+      if (lastShowedEvent) {
+        this.hideBorderBottom(lastShowedEvent)
+      }
+    }
+  }
+
+  /**
+   * It removes the class `event--hidden` from each event in the event list
+   */
+  clearFilter () {
+    this.eventList.forEach(event => {
+      event.classList.remove('event-card--hidden')
+      this.resetBorderBottom(event)
+    })
+  }
+}
+
+/**
+
+Enum for filter types.
+@readonly
+@enum {string}
+@property {string} ALL - Show all events.
+@property {string} VIRTUAL - Show virtual events only.
+@property {string} IN_PERSON - Show in-person events only.
+*/
+const filterTypes = {
+  ALL: 'all',
+  VIRTUAL: 'virtual',
+  IN_PERSON: 'in-person'
+}
+
 const getParent = function (child, className) {
   const parent = child.parentElement
   const isControlParent = parent.classList.contains(className)
@@ -105,4 +192,35 @@ document.addEventListener('DOMContentLoaded', () => {
       toggleCards(eventCard, eventCards)
     })
   })
+
+  // Events filter
+  const filterButtons = document.querySelectorAll('.event-list__filter')
+  const eventsListContainer = document.querySelector('.events')
+  const activeFilterClass = 'active'
+  /* Adding an event listener to each button in the filterButtons array. */
+  filterButtons.forEach(function (button) {
+    if (button) {
+      button.addEventListener('click', function () {
+        const { filter } = button.dataset
+        const elements = eventsListContainer.querySelectorAll('.event-card')
+        const eventList = new EventList(elements)
+
+        /* Checking if the filter is set to all, if it is, it clears the filter, if it is
+                not, it filters the list. */
+        if (filter === filterTypes.ALL) {
+          eventList.clearFilter()
+        } else {
+          eventList.filter(filter)
+        }
+
+        /* Removing the active class from all the buttons and then adding it to the button
+                that was clicked. */
+        filterButtons.forEach(button => {
+          button.classList.remove(activeFilterClass)
+        })
+        button.classList.add(activeFilterClass)
+      })
+    }
+  })
+  // Events filter
 })
